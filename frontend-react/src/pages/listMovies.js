0b7abd97cfea4179add97f71fe5790b732f75987
Sanu Khan <sanulgbello@gmail.com>
2022-01-25 23:39:45 +0400

    List movies layout

--- frontend-react/src/pages/listMovies.js
+++ frontend-react/src/pages/listMovies.js
@@ -35,8 +35,10 @@ const ListMovies = ({ movies, onDeleteMovie }) => {
                 <div className="mt-5 md:mt-0 md:col-span-2">
                     <div className="flex flex-wrap">
                         <h1 className="text-2xl font-bold text-gray-800 mx-5 my-5">List Movies</h1>
-                        <Link to="/addMovie" className="h-10 my-5 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Add New Movie
+                        <Link to="/addMovie">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-5">
+                                Add Movie
+                            </button>
                         </Link>
                     </div>
                     <div className="shadow sm:rounded-md sm:overflow-hidden">
@@ -51,53 +53,46 @@ const ListMovies = ({ movies, onDeleteMovie }) => {
                                                         #
                                                     </th>
                                                     <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                        First
+                                                        Title
                                                     </th>
                                                     <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                        Last
+                                                        Year
                                                     </th>
                                                     <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                                        Handle
+                                                        Genre
+                                                    </th>
+                                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                                        Action
                                                     </th>
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        Mark
-                                                    </td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        Otto
-                                                    </td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        @mdo
-                                                    </td>
-                                                </tr>
-                                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        Jacob
-                                                    </td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        Thornton
-                                                    </td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        @fat
-                                                    </td>
-                                                </tr>
-                                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        Larry
-                                                    </td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        Wild
-                                                    </td>
-                                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                        @twitter
-                                                    </td>
-                                                </tr>
+                                                {movies.map((movie, index) => (
+                                                    <tr class="bg-white" key={movie.id}>
+                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+                                                            {index + 1}
+                                                        </td>
+                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                                                            {movie.title}
+                                                        </td>
+                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                                                            {movie.year}
+                                                        </td>
+                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                                                            {movie.genre}
+                                                        </td>
+                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                                                            <Link to={`/editMovie/${movie.id}`} className="h-10 my">
+                                                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2">
+                                                                    View
+                                                                </button>
+                                                            </Link>
+                                                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-2" onClick={() => onDeleteMovie(movie.id)}>
+                                                                Delete
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                ))}
                                             </tbody>
                                         </table>
                                     </div>
